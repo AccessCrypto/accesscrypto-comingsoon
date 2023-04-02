4b54b9adce05b3b96a1cb9c6ea80419c22f170e5
Mole Finance <molefinance@protonmail.com>
2023-04-01 23:46:31 -0400

    added coming soon content

---
+++ src/ComingSoon.js
@@ -0,0 +1,78 @@
+export default function ComingSoon()
+{
+    return <body className="antialiased bg-body text-body font-body">
+    <div className="">
+                
+      <section className="py-10 bg-gray-50 overflow-hidden">
+        <div className="container mx-auto px-4">
+          <div className="relative pt-16 lg:pt-40 lg:pb-44 bg-white overflow-hidden rounded-3xl">
+            <div className="max-w-7xl mx-auto">
+              <div className="flex flex-wrap items-center -m-8">
+                <div className="w-full md:w-1/2 p-8 mb-16 lg:mb-0">
+                  <div className="md:max-w-xl px-8">
+      <span className="inline-block mb-5 text-sm text-blue-500 font-bold uppercase tracking-widest">ACCESS CRYPTO</span>
+                    <h2 className="font-heading mb-6 text-4xl md:text-5xl lg:text-6xl text-gray-900 font-black tracking-tight">Coming Soon</h2>
+                    <p className="mb-6 text-gray-500 font-bold">Something great will be here soon!</p>
+                    <div className="md:max-w-md">
+                      <div className="flex flex-wrap -m-2">
+                        <div className="w-auto p-2">
+                          <div className="flex flex-wrap px-3 py-2 bg-gray-100 rounded-full">
+                            <div className="w-auto mr-2 pt-1">
+                              <svg width="12" height="12" viewbox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.095 1H3.905C2.085 1 1 2.085 1 3.905V8.09C1 9.915 2.085 11 3.905 11H8.09C9.91 11 10.995 9.915 10.995 8.095V3.905C11 2.085 9.915 1 8.095 1ZM8.39 4.85L5.555 7.685C5.485 7.755 5.39 7.795 5.29 7.795C5.19 7.795 5.095 7.755 5.025 7.685L3.61 6.27C3.465 6.125 3.465 5.885 3.61 5.74C3.755 5.595 3.995 5.595 4.14 5.74L5.29 6.89L7.86 4.32C8.005 4.175 8.245 4.175 8.39 4.32C8.535 4.465 8.535 4.7 8.39 4.85Z" fill="#3B82F6"></path>
+                              </svg>
+                            </div>
+                            <div className="flex-1">
+                              <p className="text-sm text-gray-900 font-bold">Cryptocurrency</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="w-auto p-2">
+                          <div className="flex flex-wrap px-3 py-2 bg-gray-100 rounded-full">
+                            <div className="w-auto mr-2 pt-1">
+                              <svg width="12" height="12" viewbox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.095 1H3.905C2.085 1 1 2.085 1 3.905V8.09C1 9.915 2.085 11 3.905 11H8.09C9.91 11 10.995 9.915 10.995 8.095V3.905C11 2.085 9.915 1 8.095 1ZM8.39 4.85L5.555 7.685C5.485 7.755 5.39 7.795 5.29 7.795C5.19 7.795 5.095 7.755 5.025 7.685L3.61 6.27C3.465 6.125 3.465 5.885 3.61 5.74C3.755 5.595 3.995 5.595 4.14 5.74L5.29 6.89L7.86 4.32C8.005 4.175 8.245 4.175 8.39 4.32C8.535 4.465 8.535 4.7 8.39 4.85Z" fill="#3B82F6"></path>
+                              </svg>
+                            </div>
+                            <div className="flex-1">
+                              <p className="text-sm text-gray-900 font-bold">Friendly</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="w-auto p-2">
+                          <div className="flex flex-wrap px-3 py-2 bg-gray-100 rounded-full">
+                            <div className="w-auto mr-2 pt-1">
+                              <svg width="12" height="12" viewbox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.095 1H3.905C2.085 1 1 2.085 1 3.905V8.09C1 9.915 2.085 11 3.905 11H8.09C9.91 11 10.995 9.915 10.995 8.095V3.905C11 2.085 9.915 1 8.095 1ZM8.39 4.85L5.555 7.685C5.485 7.755 5.39 7.795 5.29 7.795C5.19 7.795 5.095 7.755 5.025 7.685L3.61 6.27C3.465 6.125 3.465 5.885 3.61 5.74C3.755 5.595 3.995 5.595 4.14 5.74L5.29 6.89L7.86 4.32C8.005 4.175 8.245 4.175 8.39 4.32C8.535 4.465 8.535 4.7 8.39 4.85Z" fill="#3B82F6"></path>
+                              </svg>
+                            </div>
+                            <div className="flex-1">
+                              <p className="text-sm text-gray-900 font-bold">Peer to Peer</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="w-auto p-2">
+                          <div className="flex flex-wrap px-3 py-2 bg-gray-100 rounded-full">
+                            <div className="w-auto mr-2 pt-1">
+                              <svg width="12" height="12" viewbox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.095 1H3.905C2.085 1 1 2.085 1 3.905V8.09C1 9.915 2.085 11 3.905 11H8.09C9.91 11 10.995 9.915 10.995 8.095V3.905C11 2.085 9.915 1 8.095 1ZM8.39 4.85L5.555 7.685C5.485 7.755 5.39 7.795 5.29 7.795C5.19 7.795 5.095 7.755 5.025 7.685L3.61 6.27C3.465 6.125 3.465 5.885 3.61 5.74C3.755 5.595 3.995 5.595 4.14 5.74L5.29 6.89L7.86 4.32C8.005 4.175 8.245 4.175 8.39 4.32C8.535 4.465 8.535 4.7 8.39 4.85Z" fill="#3B82F6"></path>
+                              </svg>
+                            </div>
+                            <div className="flex-1">
+                              <p className="text-sm text-gray-900 font-bold">Powerful</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+             <img className="relative z-10 md:absolute md:top-1/2 md:right-0 md:w-1/2 md:transform md:-translate-y-1/2 xl:w-auto mx-auto md:mr-0 pl-8 pb-8 md:pl-0 md:pb-0" src="zanrly-assets/images/applications/mockup.png" alt=""/>
+          </div>
+        </div>
+      </section>
+    </div>
+</body>
+}
